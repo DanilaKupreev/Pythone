042ea1ea1319519157762afa494ec90bc97e2e9a
DanilaKupreev <118535332+DanilaKupreev@users.noreply.github.com>
2022-12-12 12:15:18 +0300

    Update task_06.py

--- task_06.py
+++ task_06.py
@@ -1,32 +1,33 @@
 def rps_game_winner(x):
 	y = ["S","R","P"]
-	if x[0][1] in y and x[1][1] in y:
-		if x[0][1]=='P':
-			if x[1][1]=='R':
-				a=x[0][0]+' '+x[0][1]
-			elif x[1][1]=='P':
-				a=x[0][0]+' '+x[0][1]
-			else:
-				a=x[1][0]+' '+x[1][1]
-		if x[0][1]=='R':
-			if x[1][1]=='R':
-				a=x[0][0]+' '+x[0][1]
-			elif x[1][1]=='S':
-				a=x[0][0]+' '+x[0][1]
-			else:
-				a=x[1][0]+' '+x[1][1]
-
-		if x[0][1]=='S':
-			if x[1][1]=='S':
-				a=x[0][0]+' '+x[0][1]
-			elif x[1][1]=='P':
-				a=x[0][0]+' '+x[0][1]
-			else:
-				a=x[1][0]+' '+x[1][1]
-	else:
-		a='NoSuchStrategyError'
-		
-	if len(x)>2:
-		a='WrongNumberOfPlayersError'
 	
+	if len(x)==2:
+		if x[0][1] in y and x[1][1] in y:
+			if x[0][1]=='P':
+				if x[1][1]=='R':
+					a=x[0][0]+' '+x[0][1]
+				elif x[1][1]=='P':
+					a=x[0][0]+' '+x[0][1]
+				else:
+					a=x[1][0]+' '+x[1][1]
+			if x[0][1]=='R':
+				if x[1][1]=='R':
+					a=x[0][0]+' '+x[0][1]
+				elif x[1][1]=='S':
+					a=x[0][0]+' '+x[0][1]
+				else:
+					a=x[1][0]+' '+x[1][1]
+
+			if x[0][1]=='S':
+				if x[1][1]=='S':
+					a=x[0][0]+' '+x[0][1]
+				elif x[1][1]=='P':
+					a=x[0][0]+' '+x[0][1]
+				else:
+					a=x[1][0]+' '+x[1][1]
+		else:
+			a='NoSuchStrategyError'
+	
+	if len(x)>2 or len(x)==1:
+		a='WrongNumberOfPlayersError'
 	return a
