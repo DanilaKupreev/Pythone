4f8693c74693e24a0512e2d19e226b148f93d4d3
DanilaKupreev <118535332+DanilaKupreev@users.noreply.github.com>
2022-12-14 13:43:14 +0300

    Update task_06.py

--- task_06.py
+++ task_06.py
@@ -1,37 +1,34 @@
+class MyError(Exception):
+	pass
+
 def rps_game_winner(x):
 	y = ["S","R","P"]
-	
 	if len(x)==2:
-		try:
-			if x[0][1] in y and x[1][1] in y:
-				if x[0][1]=='P':
-					if x[1][1]=='R':
-						a=x[0][0]+' '+x[0][1]
-					elif x[1][1]=='P':
-						a=x[0][0]+' '+x[0][1]
-					else:
-						a=x[1][0]+' '+x[1][1]
-				if x[0][1]=='R':
-					if x[1][1]=='R':
-						a=x[0][0]+' '+x[0][1]
-					elif x[1][1]=='S':
-						a=x[0][0]+' '+x[0][1]
-					else:
-						a=x[1][0]+' '+x[1][1]
+		if x[0][1] in y and x[1][1] in y:
+			if x[0][1]=='P':
+				if x[1][1]=='R':
+					a=x[0][0]+' '+x[0][1]
+				elif x[1][1]=='P':
+					a=x[0][0]+' '+x[0][1]
+				else:
+					a=x[1][0]+' '+x[1][1]
+			if x[0][1]=='R':
+				if x[1][1]=='R':
+					a=x[0][0]+' '+x[0][1]
+				elif x[1][1]=='S':
+					a=x[0][0]+' '+x[0][1]
+				else:
+					a=x[1][0]+' '+x[1][1]
 
-				if x[0][1]=='S':
-					if x[1][1]=='S':
-						a=x[0][0]+' '+x[0][1]
-					elif x[1][1]=='P':
-						a=x[0][0]+' '+x[0][1]
-					else:
-						a=x[1][0]+' '+x[1][1]
+			if x[0][1]=='S':
+				if x[1][1]=='S':
+					a=x[0][0]+' '+x[0][1]
+				elif x[1][1]=='P':
+					a=x[0][0]+' '+x[0][1]
+				else:
+					a=x[1][0]+' '+x[1][1]
 			return a
-		except:
-			return "NoSuchStrategyError"
-		
+		else:
+			raise MyError("NoSuchStrategyError")
 	if len(x)!=2:
-		try:
-			raise "WrongNumberOfPlayersError"
-		except:
-			return "WrongNumberOfPlayersError"
+		raise MyError("WrongNumberOfPlayersError")
